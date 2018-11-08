734d993ef1365e864b04867cf59a831b06a5c082
zfc <fengcheng.zhang@percent.cn>
2018-11-08 19:21:01 +0800

    init all

---
+++ app/src/main/java/com/brother6/android_frame_aop/MainActivity.java
@@ -0,0 +1,13 @@
+package com.brother6.android_frame_aop;
+
+import android.support.v7.app.AppCompatActivity;
+import android.os.Bundle;
+
+public class MainActivity extends AppCompatActivity {
+
+    @Override
+    protected void onCreate(Bundle savedInstanceState) {
+        super.onCreate(savedInstanceState);
+        setContentView(R.layout.activity_main);
+    }
+}
