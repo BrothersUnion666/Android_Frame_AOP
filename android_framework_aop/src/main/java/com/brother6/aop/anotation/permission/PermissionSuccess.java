ca2c9d92c73dc1547c22389a84c7ce876ecdef04
zfc <fengcheng.zhang@percent.cn>
2018-11-12 06:34:49 +0800

    添加上注解的应用类型

--- android_framework_aop/src/main/java/com/brother6/aop/anotation/permission/PermissionSuccess.java
+++ android_framework_aop/src/main/java/com/brother6/aop/anotation/permission/PermissionSuccess.java
@@ -1,5 +1,12 @@
 package com.brother6.aop.anotation.permission;
 
+import java.lang.annotation.ElementType;
+import java.lang.annotation.Retention;
+import java.lang.annotation.RetentionPolicy;
+import java.lang.annotation.Target;
+
+@Retention(RetentionPolicy.RUNTIME)
+@Target(ElementType.METHOD)
 public @interface PermissionSuccess {
     //获取权限的注解
     int requestCode();
